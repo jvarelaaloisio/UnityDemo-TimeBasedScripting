9cf8e1ef2669446b160005fbd55e223474ad5051
jvarelaaloisio <juampyvarela@gmail.com>
2025-04-23 10:54:08 -0300

    Added AnimateText and expanded AnimateCube for coroutines

--- Assets/- Coroutines -/AnimateCube.cs
+++ Assets/- Coroutines -/AnimateCube.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using UnityEngine;
 
@@ -7,13 +8,17 @@ namespace Coroutines
     {
         [SerializeField] private float speed = 10;
         [SerializeField] private Transform cube;
-        
-        //Coroutines are automatically disabled when Components are disabled.
-        //Also, components are disabled when their GameObject is deactivated.
+
+        private void Reset() => cube = transform;
+
+        //Coroutines are automatically stopped when their GameObject is deactivated,
+        //but not when their component is just disabled.
+        //To have that logic, you can modify the While statement to While(enabled){} 
         private void OnEnable() => StartCoroutine(Rotate());
 
         private IEnumerator Rotate()
         {
+            //destroyCancellationToken is an object that can request a cancellation on a process from the outside.
             while (!destroyCancellationToken.IsCancellationRequested)
             {
                 var t = Time.time * (speed * Time.deltaTime);
--- Assets/- Coroutines -/AnimateText.cs
+++ Assets/- Coroutines -/AnimateText.cs
@@ -1,9 +1,68 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 namespace Coroutines
 {
     public class AnimateText : MonoBehaviour
     {
+        private const string SizePrefix = "<size={0}%>";
+        private const string SizeSuffix = "</size>";
+        [SerializeField] private TMP_Text label;
+        [SerializeField] private short maxScale = 125;
+        [SerializeField] private float minScale = 100;
+        [SerializeField] private short speed = 200;
+        private string _originalText;
+        private float _previousCharScale;
+        private float _currentCharScale;
+        private short _currentChar;
+
+        private void Reset() => label = GetComponent<TMP_Text>();
+
+        private void Awake()
+        {
+            _originalText = label.text;
+            _previousCharScale = minScale;
+            _currentCharScale = minScale;
+        }
         
+        private void OnEnable() => StartCoroutine(Rotate());
+
+        private IEnumerator Rotate()
+        {
+            while (true)
+            {
+                var sizeDelta = Time.deltaTime * speed;
+                _currentCharScale += sizeDelta;
+                _previousCharScale -= sizeDelta;
+                
+                if (_previousCharScale <= minScale)
+                {
+                    if (++_currentChar > _originalText.Length)
+                        _currentChar = 0;
+                    _previousCharScale = maxScale;
+                    _currentCharScale = minScale;
+                }
+
+                var previousCharIndex = Mathf.Max(0, _currentChar - 1);
+                var currentCharIndex = Mathf.Min(_originalText.Length - 1, _currentChar);
+            
+                var previousChar = _currentChar <= 0
+                                       ? string.Empty
+                                       : GetSizePrefix(_previousCharScale) + _originalText[previousCharIndex] + SizeSuffix;
+            
+                var currentChar = _currentChar >= _originalText.Length
+                                      ? string.Empty
+                                      :GetSizePrefix(_currentCharScale) + _originalText[currentCharIndex] + SizeSuffix;
+            
+                var prefix = _originalText[..previousCharIndex];
+                var suffix = _originalText[(currentCharIndex + 1)..];
+                label.text = $"{prefix}{previousChar}{currentChar}{suffix}";
+                
+                yield return null;
+            }
+        }
+        private static string GetSizePrefix(float scale)
+            => string.Format(SizePrefix, Mathf.Floor(scale));
     }
 }
